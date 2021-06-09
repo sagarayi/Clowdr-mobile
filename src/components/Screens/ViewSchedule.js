582a312d7399e4cfffb421de950d923fb9f21c97
Sagar Ayi <ayisagar@gmail.com>
2021-06-09 14:49:58 -0400

    Added new plugin for event viewing

--- src/components/Screens/ViewSchedule.js
+++ src/components/Screens/ViewSchedule.js
@@ -1,28 +1,35 @@
 //ViewSchedule.js
 import React, { Component } from "react";
 import { View, Button, Alert } from "react-native";
+import EventCalendar from 'react-native-events-calendar'
 import { Calendar } from 'react-native-big-calendar'
 
 import AppButton from "../common/AppButton";
-
-const events = [
-    {
-      title: 'Meeting',
-      start: new Date(2021, 5, 4, 11, 0),
-      end: new Date(2021, 5, 4, 11, 30)
-    },
-    {
-      title: 'Coffee break',
-      start: new Date(2020, 1, 11, 15, 45),
-      end: new Date(2020, 1, 11, 16, 30),
-    },
-  ]
+  const events = [
+    { start: '2021-06-09 00:30:00', end: '2021-06-09 01:30:00', title: 'Dr. Mariana Joseph', summary: '3412 Piedmont Rd NE, GA 3032' },
+    { start: '2021-06-09 01:30:00', end: '2021-06-09 02:20:00', title: 'Dr. Mariana Joseph', summary: '3412 Piedmont Rd NE, GA 3032' },
+    { start: '2021-06-09 04:10:00', end: '2021-06-09 04:40:00', title: 'Dr. Mariana Joseph', summary: '3412 Piedmont Rd NE, GA 3032' },
+    { start: '2021-06-09 01:05:00', end: '2021-06-09 01:45:00', title: 'Dr. Mariana Joseph', summary: '3412 Piedmont Rd NE, GA 3032' },
+    { start: '2021-06-09 14:30:00', end: '2021-06-09 16:30:00', title: 'Dr. Mariana Joseph', summary: '3412 Piedmont Rd NE, GA 3032' },
+    { start: '2021-06-09 14:20:00', end: '2021-06-09 16:20:00', title: 'Dr. Mariana Joseph', summary: '3412 Piedmont Rd NE, GA 3032' }
+]
 
 export default class ViewSchedule extends React.Component {
 
+    componentDidMount() {
+        this.setState({
+            events:[]
+        })
+
+    }
+
+    fetchEventsForDate(){
+
+    }
     render() {
-        return <View>
-            <Calendar events={events} height={600} mode="day" />
-        </View>
+        return <EventCalendar
+        events={events}
+        width={400}
+      />
     }
 }
