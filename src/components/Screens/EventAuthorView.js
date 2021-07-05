28548a4449f07c455a1b744180ef30ead5f6b579
Sagar Ayi <ayisagar@gmail.com>
2021-07-05 11:35:47 -0400

    Added some UI elements to the details screen

--- src/components/Screens/EventAuthorView.js
+++ src/components/Screens/EventAuthorView.js
@@ -1,17 +1,37 @@
 import React from "react";
 import { useQuery } from '@apollo/client';
 import EventCalendar from 'react-native-events-calendar'
-import { Text, View } from "react-native";
+import { Text, View, Image, StyleSheet } from "react-native";
 import * as Constants from "../common/Constants";
 import * as Queries from "../common/GraphQLQueries";
 import AppButton from "../common/AppButton";
 
 
+const styles = StyleSheet.create({
+    tinyLogo: {
+      width: 20,
+      height: 20,
+      top: 25,
+      marginLeft: 10
+    },
+    authorName: {
+        left: 30,
+        marginLeft: 10
+    },
+    authorDetails: {
+        left: 30,
+        marginLeft: 10
+    }
+  });
+
 export default function EventAuthorView({name, institution, role}){
     
     return <View>
-        <Text>{name}</Text>
-        <Text>{role}</Text>
-        <Text>{institution}</Text>
+        <Image style={styles.tinyLogo}
+        source={require("../../../assets/avatar.png")} />
+        <Text style={styles.authorName}>{name}</Text>
+        <Text style={styles.authorDetails}>{role}</Text>
+        <Text style={styles.authorDetails}>{institution}</Text>
+        <Text>{Constants.lineBreak}</Text>
     </View>
 }
--- src/components/Screens/PresentationEvent.js
+++ src/components/Screens/PresentationEvent.js
@@ -1,12 +1,27 @@
 import React from "react";
 import { useQuery } from '@apollo/client';
 import EventCalendar from 'react-native-events-calendar'
-import { Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import * as Constants from "../common/Constants";
 import * as Queries from "../common/GraphQLQueries";
 import AppButton from "../common/AppButton";
 import EventAuthorView from "./EventAuthorView";
 
+
+const styles = StyleSheet.create({
+    heading: {
+        fontSize: 20,
+        fontWeight: "bold",
+        left: 10,
+        right: 10
+    },
+
+    abstractText: {
+        marginLeft: 10,
+        marginRight: 10,
+        textAlign: "justify"
+    }
+})
 
 export default function PresentationEvent({route, navigation}) {
 
@@ -17,11 +32,14 @@ export default function PresentationEvent({route, navigation}) {
     }) 
     const authors = [1, 2, 3]
 
-    return <View>
+    return <ScrollView>
         <AppButton title="Discussion Room" onPress={()=>{alert("this will join the presentation")}}/>
         {authors.map((author) => {
             // alert(author)
            return <EventAuthorView name={"Author name"+author} institution="Northeastern" role="Author" />
         })}
-    </View>
+        <Text>{Constants.lineBreak}</Text>
+        <Text style={styles.heading}>Abstract</Text>
+        <Text style={styles.abstractText}>{Constants.loremIpsumText}</Text>
+    </ScrollView>
 }
