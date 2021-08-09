1666929359af6243625689d60c235003edcc2c0f
Sagar Ayi <ayisagar@gmail.com>
2021-08-09 13:14:58 -0400

    Basic websocket connection is working

--- src/components/Screens/DetailedChatView.js
+++ src/components/Screens/DetailedChatView.js
@@ -16,6 +16,7 @@ import Config from "react-native-config";
 import io from "socket.io-client";
 import { graphql } from "graphql";
 import { useState } from "react";
+import { useEffect } from "react";
 
 const styles = StyleSheet.create({
     container: {
@@ -32,15 +33,11 @@ const styles = StyleSheet.create({
     }
   });
 
-const fetchAccessToken = async() => {
-    const accessToken = await AsyncStorage.getItem(Constants.ACCESS_TOKEN);
-    console.log(accessToken)
-    return accessToken
-}
+
 
 const fetchCurrentUserId= async() =>{
     const userId = await AsyncStorage.getItem(Constants.USER_ID)
-    console.log("User id : "+ userId)
+    // console.log("User id : "+ userId)
     return userId
   };
 
@@ -55,13 +52,13 @@ function getMessageObject(chatId, message, senderId) {
                     senderId: senderId,
                     type: Constants.ChatMessageType.Message,
                     data: "",
-                    isPinned: "false",
+                    isPinned: "true",
                 }
     const action = {
         op: "INSERT",
         data: newMsg,
     };
-    console.log("Constructed object: "+ JSON.stringify(action))
+    // console.log("Constructed object: "+ JSON.stringify(action))
     return action
 }
 
@@ -87,11 +84,11 @@ function getMessageObjectFromResponse(response) {
         return {}
     }
     const data = res.data
-    console.log("response parsed  : "+ JSON.stringify(data))
-    console.log("response parsed sid : "+ data.sId)
-    console.log("response parsed message : "+ data.message)
+    // console.log("response parsed  : "+ JSON.stringify(data))
+    // console.log("response parsed sid : "+ data.sId)
+    // console.log("response parsed message : "+ data.message)
     // console.log("response parsed senderId : "+ data.senderId)
-    console.log("response parsed created_at : "+ data.created_at)
+    // console.log("response parsed created_at : "+ data.created_at)
     const msg =     {
         sId: data.sId,
         message: data.message,
@@ -106,37 +103,82 @@ function getMessageObjectFromResponse(response) {
 
 export default function DetailedChatView({route, navigation}) {
 
-    console.log("route : "+ JSON.stringify(route))
-    const chatId = route.params.chatId
+    const fetchAccessToken = async() => {
+        const accessToken = await AsyncStorage.getItem(Constants.ACCESS_TOKEN);
+        // console.log(accessToken)
+        return JSON.stringify(accessToken)
+    }
+    // var client = null
+    // useEffect(() => {
+
+    // })
+
+    // console.log("route : "+ JSON.stringify(route))
+    
     const title = route.params.chatTitle
 
     // const socket = io("http://localhost:1234");
 
-    const token = ""
-    // fetchAccessToken()
+    const token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImJOeW9NQ1BtLWpzeWVFY2JPVDRkRyJ9.eyJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWRlZmF1bHQtcm9sZSI6InVzZXIiLCJ4LWhhc3VyYS1hbGxvd2VkLXJvbGVzIjpbInVzZXIiLCJ1bmF1dGhlbnRpY2F0ZWQiXSwieC1oYXN1cmEtdXNlci1pZCI6Imdvb2dsZS1vYXV0aDJ8MTEyNTMyMDQyMTc5MTM5MDQzMzYwIiwieC1oYXN1cmEtY29uZmVyZW5jZS1zbHVncyI6IntcIm1hYzIwMjFcIixcIm1hYzIwMjJcIn0ifSwiaXNzIjoiaHR0cHM6Ly9kZXYtampyYmZoeDUudXMuYXV0aDAuY29tLyIsInN1YiI6Imdvb2dsZS1vYXV0aDJ8MTEyNTMyMDQyMTc5MTM5MDQzMzYwIiwiYXVkIjpbImhhc3VyYSIsImh0dHBzOi8vZGV2LWpqcmJmaHg1LnVzLmF1dGgwLmNvbS91c2VyaW5mbyJdLCJpYXQiOjE2Mjg1MjQzMTMsImV4cCI6MTYyODYxMDcxMywiYXpwIjoiQ2RJa2hidm1nRDRLbFFnV21TeXFpZW1kUUFiT2lQNloiLCJzY29wZSI6Im9wZW5pZCJ9.ZsVlNpZFhAAyB39R6BGLJMNZXBeHl87HXIesCcWh_GfInh0x5-cITPVLnaRWsiqRR8oAU397gNmK4uCSNaVKpGH5jCPvsWMbWfniW-ebq-6bclRbZnUYfzigtzypMMTU-STb4wEAwDJATcR6PB5zGqRcA_liNcwkqVJsShCzGQQKZC6kO4ZLc1zn3VtdGsyuuTV4PYHD3o5Os-euzia8rwPKPgcPSce7VOIC0J8i4cBuQhcLQ60i6uQLtjYA2pJEVhCkfEHpTz1uGm4xJPIqaZBtorj0iFr1-r15b0fHOjL3cGp07gChmH7BzmnAonIvQBXg-L63T86NkojH-1e8-g'
+    // console.log("Token: ",token)
+    // 
     const userId =  ""
     // fetchCurrentUserId()
-    console.log("Access token : "+token)
-    // const client = io(Config.WEBSOCKET_SERVER_URL, {
-    //     auth: {
-    //         token,
-    //     },
-    //     transports: ["websocket"],
-    // });
+    // console.log("Access token : "+token)
+
     // io.connect(Config.WEBSOCKET_SERVER_URL)
 
+    console.log(client)
     const [messages, setMessages] = useState([])
-    console.log("Messages : ", messages)
+    // console.log("Messages : ", messages)
 
-    console.log(Config.WEBSOCKET_SERVER_URL)
+    // console.log(Config.WEBSOCKET_SERVER_URL)
+
+    const uuid = uuidv4()
+    
+    // client.emit('chat.messages.send', {});
 
     // client.on("connect", () => {
     //     console.log("Socket connected")
     // })
 
-    // client.on("chat.subscribe", () => {
-    //     console.log("Chat subscribed ")
-    // })
+    const chatId = route.params.chatId
+    // const server_url = "ws://localhost:3002"
+    // var ws = new WebSocket('ws://localhost:3002');
+    // console.log(ws)
+    // ws.send("Test")
+    // ws.onopen = () => {
+    //     // connection opened
+    //     console.log("It connected !!")
+    //   };
+
+    const client = io(Config.WEBSOCKET_SERVER_URL, {
+        auth: {
+            token,
+        },
+        transports: ["websocket"],
+    });
+    // console.log("Auth : "+ JSON.stringify(client.auth))
+    client.emit("chat.subscribe", chatId);
+    client.emit("chat.messages.send", getMessageObject(uuidv4(), "test", uuidv4()));
+    
+    // console.log(client.io.)
+    client.on("chat.subscribed", () => {
+        console.log("Chat subscribed ")
+        
+    })
+
+    client.on("chat.messages.send.ack", () => {
+        console.log("Chat message sent ")
+    })
+
+    client.on("connection", () => {
+        console.log("Connected")
+    })
+
+    client.on("chat:chat."+userId, () => {
+        console.log("Notification on")
+    })
 
 
     // client.emit("chat.subscribe", uuid);
@@ -179,7 +221,7 @@ export default function DetailedChatView({route, navigation}) {
         // const uuid = uuidv4()
         
         // const message = getMessageObject(chatId, chatMessage, userId)
-        // client.emit("chat.subscribe", uuid);
+        
         // client.emit("chat.messages.send", message)
         // console.log("Sent?? "+uuid)
         // socket.emit('chat message', message);
@@ -198,7 +240,7 @@ export default function DetailedChatView({route, navigation}) {
     // var chatMessages = messages.map(msgInfo => (
     //     <MessageView messageInfo={msgInfo}/>
     //   ));
-    console.log("Camer here ")
+    // console.log("Camer here ")
     navigation.setOptions({
         headerTitle: title
     }) 
